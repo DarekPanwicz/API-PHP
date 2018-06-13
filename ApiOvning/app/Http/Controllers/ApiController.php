f3a4270517b0c4623abb92c10e9db16b61d1c1ea
dajmos <dajmos007@gmail.com>
2018-06-13 14:17:57 +0200

    Taking json data from users API, converting json in to php object

--- ApiOvning/app/Http/Controllers/ApiController.php
+++ ApiOvning/app/Http/Controllers/ApiController.php
@@ -8,6 +8,21 @@ class ApiController extends Controller
 {
     public function getApi()
     {
-        return view('hem');
+
+        $url = 'https://jsonplaceholder.typicode.com/users';
+        $data = file_get_contents($url);
+        $apiUsers = json_decode($data);
+
+
+
+
+
+
+        return view('hem', compact('apiUsers'));
+
+
+
+
+
     }
 }
