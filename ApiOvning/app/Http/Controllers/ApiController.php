74dbc916c0a98f1a5c36c0cfc6a2affaabfd67c8
dajmos <dajmos007@gmail.com>
2018-06-13 14:28:32 +0200

    Adding new api Getaway

--- ApiOvning/app/Http/Controllers/ApiController.php
+++ ApiOvning/app/Http/Controllers/ApiController.php
@@ -8,17 +8,27 @@ class ApiController extends Controller
 {
     public function getApi()
     {
-
+        // API: lista alla användare
         $url = 'https://jsonplaceholder.typicode.com/users';
         $data = file_get_contents($url);
         $apiUsers = json_decode($data);
 
+        // API: Task för en specifik användare
+        $url = 'https://jsonplaceholder.typicode.com/todos?userId=1';
+        $data = file_get_contents($url);
+        $apiTodo = json_decode($data);
+
+        // API: genomförda Task
+        $url = 'https://jsonplaceholder.typicode.com/todos?completed=true';
+        $data = file_get_contents($url);
+        $apiFinishTask = json_decode($data);
 
 
 
 
 
-        return view('hem', compact('apiUsers'));
+
+        return view('hem', compact('apiUsers', 'apiTodo', 'apiFinishTask'));
 
 
 
